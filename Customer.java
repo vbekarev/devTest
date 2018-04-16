23bd699d00cb66dedf66766f692f3cf7891d7bdc
vbekarev <vlbekarev@gmail.com>
2018-04-17 00:01:17 +0700

    Adding class Customer.java

---
+++ Customer.java
@@ -0,0 +1,38 @@
+package net.proselyte.gittutorial.company;
+
+import java.util.Set;
+
+/**
+ * Simple JavaBean domain object that represents a Customer
+ *
+ * @author Eugene Suleimanov
+ * @version 1.0
+ */
+public class Customer {
+    private String name;
+    private Set projects;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", projects=" + projects +
+                '}';
+    }
+}
